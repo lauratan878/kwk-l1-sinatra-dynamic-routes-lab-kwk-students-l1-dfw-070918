5363127fc6be36da074132a01dd60784c66f8620
lauratan878 <lauratan878@gmail.com>
2018-07-19 15:57:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  
+get "/hello" do
+  
 end
+get "/hello name" do
+  @user_name = params[:name]
+  "Hello #{@user_name}"
+end 
+get "/goodbye name" do
+  @user_name = params[:name]
+  "Goodbye #{@user_name}"
+end
+end 
